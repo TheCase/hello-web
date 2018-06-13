c7d8acb89c01ac75b7360ca068e459e6bd5a4d8a
TheCase <github@repulsor.net>
2018-06-13 16:55:30 -0600

    n# Please enter the commit message for your changes. Lines starting

--- gitvars.sh
+++ gitvars.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-export CI=true
-export CB=true
+CI=true
+CB=true
 
-export CB_GIT_BRANCH=`git symbolic-ref HEAD --short 2>/dev/null`
+CB_GIT_BRANCH=`git symbolic-ref HEAD --short 2>/dev/null`
 if [ "$CB_GIT_BRANCH" == "" ] ; then
   CB_GIT_BRANCH=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
-  export CB_GIT_BRANCH=${CB_GIT_BRANCH#remotes/origin/}
+  CB_GIT_BRANCH=${CB_GIT_BRANCH#remotes/origin/}
 fi
 
-export CB_GIT_MESSAGE=`git log -1 --pretty=%B`
-export CB_GIT_AUTHOR=`git log -1 --pretty=%an`
-export CB_GIT_AUTHOR_EMAIL=`git log -1 --pretty=%ae`
-export CB_GIT_COMMIT=`git log -1 --pretty=%H`
-export CB_GIT_TAG=`git describe --tags --abbrev=0`
+CB_GIT_MESSAGE=`git log -1 --pretty=%B`
+CB_GIT_AUTHOR=`git log -1 --pretty=%an`
+CB_GIT_AUTHOR_EMAIL=`git log -1 --pretty=%ae`
+CB_GIT_COMMIT=`git log -1 --pretty=%H`
+CB_GIT_TAG=`git describe --tags --abbrev=0`
 
-export CB_PULL_REQUEST=false
+CB_PULL_REQUEST=false
 if [[ $CB_GIT_BRANCH == pr-* ]] ; then
-  export CB_PULL_REQUEST=${CB_GIT_BRANCH#pr-}
+  CB_PULL_REQUEST=${CB_GIT_BRANCH#pr-}
 fi
 
-export CB_PROJECT=${CB_BUILD_ID%:$CB_LOG_PATH}
-export CB_BUILD_URL=https://$AWS_DEFAULT_REGION.console.aws.amazon.com/codebuild/home?region=$AWS_DEFAULT_REGION#/builds/$CB_BUILD_ID/view/new
+CB_PROJECT=${CB_BUILD_ID%:$CB_LOG_PATH}
+CB_BUILD_URL=https://$AWS_DEFAULT_REGION.console.aws.amazon.com/codebuild/home?region=$AWS_DEFAULT_REGION#/builds/$CB_BUILD_ID/view/new
 
 echo "==> AWS CodeBuild Extra Environment Variables:"
 echo "==> CI = $CI"

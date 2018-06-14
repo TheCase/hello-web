afb8283b06f6dc7cfb1bfcecc1a789b04c6e2fcc
Bryan Casey <github@repulsor.net>
2018-06-14 16:11:14 -0600

    fix release/latest tagging

--- codebuild/post_build.sh
+++ codebuild/post_build.sh
@@ -4,10 +4,18 @@
 source codebuild/env.sh
 
 if [ "$GIT_BRANCH" == "master" ]; then
+  echo "Pusing master as latest..."
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_TAG
+  TAG_REF=`git showref --tags $GIT_TAG | awk {'print $1'}`
+  echo "# TAG_REF = $TAG_REF"
+  if [ "$TAG_REF" == "$GIT_COMMIT"]; then
+    echo "Pusing $GIT_TAG..."
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_TAG
+  fi 
 else
+  echo "Pusing $GIT_BRANCH..."
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH
+  echo "Pusing $GIT_BRANCH-$GIT_SHA..."
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH-$GIT_SHA
 
 fi

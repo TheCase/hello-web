09e71114cffe62bf029b7a566710e3d97d9cdc24
TheCase <github@repulsor.net>
2018-06-14 10:08:57 -0600

    build in script

---
+++ codebuild/post_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# have to do this in a script because the env vars are not being 
+# held in the environment even thoughthe 0.2 buildspec says they should
+source codebuild/env.sh
+
+docker build -t $IMAGE_REPO_NAME:$GIT_BRANCH .
+
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH-$SHA

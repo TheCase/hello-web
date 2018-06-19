8fca5e9152f3d8e5e1b1c62e63262703d436ced3
TheCase <github@repulsor.net>
2018-06-19 13:47:46 -0600

    typo

--- codebuild/pull-cache.sh
+++ codebuild/pull-cache.sh
@@ -3,4 +3,4 @@
 # held in the environment even thoughthe 0.2 buildspec says they should
 source codebuild/env.sh
 
-docker pull $IMAGE_REPO_NAME:$GIT_BRANCH $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH || true
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH || true

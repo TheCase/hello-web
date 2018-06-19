a53c30de5fc8fb4e3afbec3c0bd0875c8e638db9
TheCase <github@repulsor.net>
2018-06-19 13:49:39 -0600

    typo2

--- codebuild/build.sh
+++ codebuild/build.sh
@@ -3,7 +3,7 @@
 # held in the environment even thoughthe 0.2 buildspec says they should
 source codebuild/env.sh
 
-docker build --cache-from $IMAGE_REPO_NAME:$GIT_BRANCH $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH -t $IMAGE_REPO_NAME:$GIT_BRANCH .
+docker build --cache-from $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH -t $IMAGE_REPO_NAME:$GIT_BRANCH .
 if [ "$GIT_BRANCH" == "master" ]; then 
   docker tag $IMAGE_REPO_NAME:$GIT_BRANCH $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:master
   docker tag $IMAGE_REPO_NAME:$GIT_BRANCH $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:latest

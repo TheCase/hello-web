7f4b51523332b1726af405b58871b65d46ed92f1
TheCase <github@repulsor.net>
2018-06-19 13:46:09 -0600

    pull cache from repo

---
+++ codebuild/pull-cache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# have to do this in a script because the env vars are not being 
+# held in the environment even thoughthe 0.2 buildspec says they should
+source codebuild/env.sh
+
+docker pull $IMAGE_REPO_NAME:$GIT_BRANCH $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH || true

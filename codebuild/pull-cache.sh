80c43c3f21ea7959d83d5d804df9f85d798ddbc1
TheCase <github@repulsor.net>
2018-06-19 15:28:02 -0600

    fix build cache

--- codebuild/pull-cache.sh
+++ codebuild/pull-cache.sh
@@ -3,4 +3,4 @@
 # held in the environment even thoughthe 0.2 buildspec says they should
 source codebuild/env.sh
 
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$GIT_BRANCH || true
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:_build-cache || true
